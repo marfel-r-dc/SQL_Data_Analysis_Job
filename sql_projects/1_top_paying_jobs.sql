1b096b533db814d7cd1c0cb0a4eaecfd52a0257e
Marfel <marfel.r.dc@gmail.com>
2024-10-13 17:32:47 +0800

    - update

--- sql_projects/1_top_paying_jobs.sql
+++ sql_projects/1_top_paying_jobs.sql
@@ -26,23 +26,23 @@ FROM
 WHERE 
   table_name = 'job_postings_fact';
 
-
-SELECT *
+--- sql code - 
+SELECT	
+	job_id,
+	job_title,
+	job_location,
+	job_schedule_type,
+	salary_year_avg,
+	job_posted_date,
+    name AS company_name
 FROM
     job_postings_fact
-LIMIT 100
-; 
-
-
-SELECT
-    job_id,
-    job_title_short,
-    salary_hour_avg
-
-
-FROM
-    job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+    job_title_short = 'Data Analyst' AND 
+    job_location = 'Anywhere' AND 
+    salary_year_avg IS NOT NULL
 ORDER BY
     salary_year_avg DESC
-LIMIT 10
+LIMIT 10;
 ;
